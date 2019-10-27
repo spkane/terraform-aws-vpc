da14e5fb005a858a3e10f99d8f14da6f43f3aff3
Sean P. Kane <skane@newrelic.com>
2019-10-27 14:52:57 -0700

    Minor tweaks

--- .ci/bin/terraform.sh
+++ .ci/bin/terraform.sh
@@ -3,26 +3,30 @@
 TARGET_DIR=/opt
 PATH=${PATH}:${TARGET_DIR}
 
-TERRAFORM_VERSION=${1:-"0.12.6"}
-OS=${2:-"linux"}
+TERRAFORM_VERSION=${1:-"0.12.12"}
+OS=${2:-"darwin"}
 TERRAFORM_URL="https://releases.hashicorp.com/terraform/${TERRAFORM_VERSION}/terraform_${TERRAFORM_VERSION}_${OS}_amd64.zip"
+TERRAFORM_COMMAND="${TARGET_DIR}/terraform-${TERRAFORM_VERSION}"
 
 installTerraform() {
   echo "Downloading terraform: ${TERRAFORM_URL}"
 
-  curl '-#' -fL -o ${TARGET_DIR}/terraform.zip ${TERRAFORM_URL} && \
-    unzip -q -d ${TARGET_DIR}/ ${TARGET_DIR}/terraform.zip && \
-
-  terraform --version
+  cd /tmp || exit
+  rm -f /tmp/terraform
+  rm -f /tmp/terraform.zip
+  curl '-#' -fL -o /tmp/terraform.zip ${TERRAFORM_URL}
+  unzip -q -d /tmp /tmp/terraform.zip
+  cp /tmp/terraform ${TARGET_DIR}/terraform-${TERRAFORM_VERSION}
+  ${TARGET_DIR}/terraform-${TERRAFORM_VERSION} --version
 }
 
 verifyModulesAndPlugins() {
   echo "Verify plugins and modules can be resolved in $PWD"
-  terraform init -get -backend=false -input=false
+  ${TERRAFORM_COMMAND} init -get -backend=false -input=false
 }
 
 formatCheck() {
-  RESULT=$(terraform fmt -write=false)
+  RESULT=$(${TERRAFORM_COMMAND} fmt -write=false)
   if [[ ! -z ${RESULT} ]] ; then
     echo The following files are formatted incorrectly: $RESULT
     exit 1
@@ -31,6 +35,6 @@ formatCheck() {
 
 validate() {
   echo "Validating and checking format of terraform code in $PWD"
-  terraform validate
+  ${TERRAFORM_COMMAND} validate
   formatCheck
 }
